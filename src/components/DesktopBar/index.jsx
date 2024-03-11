76327f40fc12e285741bf4d1631e478dac234e2f
Vishalgiri8767 <vishalgiri8767@gmail.com>
2024-03-11 19:36:08 +0530

    initial commit

--- src/components/DesktopBar/index.jsx
+++ src/components/DesktopBar/index.jsx
@@ -7,18 +7,19 @@ export default function DesktopBar({
   ...props
 }) {
   return (
-    <div {...props}>
-      <div className="flex flex-row sm:flex-col justify-start w-[42%] mt-0.5 gap-[11px] px-[3px] sm:gap-5">
-        <Text as="p" className="mt-px sm:mt-0 !text-white-A700_99 !font-dmsans">
-          {thispageis}
-        </Text>
-        <div className="flex flex-row justify-start items-center w-[34%] sm:w-full gap-1">
-          <Text as="p" className="!text-white-A700 !font-dmsans">
-            {viewthe}
-          </Text>
-          <Img src="images/img_icons.svg" alt="icons_one" className="h-[16px] w-[16px]" />
-        </div>
-      </div>
-    </div>
+    <></>
+    // <div {...props}>
+    //   <div className="flex flex-row sm:flex-col justify-start w-[42%] mt-0.5 gap-[11px] px-[3px] sm:gap-5">
+    //     <Text as="p" className="mt-px sm:mt-0 !text-white-A700_99 !font-dmsans">
+    //       {thispageis}
+    //     </Text>
+    //     <div className="flex flex-row justify-start items-center w-[34%] sm:w-full gap-1">
+    //       <Text as="p" className="!text-white-A700 !font-dmsans">
+    //         {viewthe}
+    //       </Text>
+    //       <Img src="images/img_icons.svg" alt="icons_one" className="h-[16px] w-[16px]" />
+    //     </div>
+    //   </div>
+    // </div>
   );
 }
